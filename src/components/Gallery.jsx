60973b9ca2cc35a02d5133fec82f57e532091f3a
Himanshu <himanshurajput6654@gmail.com>
2023-06-25 13:34:18 +0530

    making routes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 function Gallery() {
   return (
     <div>
+        <Header />
       <h1 style={{color:"red"}}> Gallery </h1>
     </div>
   )
