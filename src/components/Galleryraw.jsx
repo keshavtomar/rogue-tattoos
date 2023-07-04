60118c7f28fcb4c258c4f0745197a16f4c1af331
keshav <tomarkeshavofficial@gmail.com>
2023-07-04 12:24:18 +0530

    gallery done good

--- src/components/Galleryraw.jsx
+++ src/components/Galleryraw.jsx
@@ -1,54 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/galleryraw.css';
-import { Skeleton } from '@mui/material';
-
-const ImageWithLoader = ({ src, alt }) => {
-    const [isLoading, setIsLoading] = useState(true);
-
-    const handleImageLoad = () => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 500);
-    };
-
-    return (
-        <div className='pics'>
-            {isLoading ? (
-                <Skeleton
-                    animation='pulse'
-                    sx={{ bgcolor: 'white' }}
-                    variant='rectangular'
-                    width={210}
-                    height={118}
-                />
-            ) : null}
-            <img
-                data-src={src}
-                alt={alt}
-                style={{ width: '100%' }}
-                ref={lazyLoadRef}
-                onLoad={handleImageLoad}
-            />
-        </div>
-    );
-};
 
 export default function Galleryraw() {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        const fetchImages = async () => {
+        const fetchImages = () => {
             const fetchedImages = [];
-
             for (let i = 1; i <= 100; i++) {
                 const src = `images/gallery/rogue-tattoos-gallery${i}.jpg`;
                 const id = i;
-                const image = (
-                    <ImageWithLoader key={id} src={src} alt={`rogue-tattoos${i}`} />
+                fetchedImages.push(
+                    <div className='pics' key={id}>
+                        <img
+                            data-src={src}
+                            alt={`rogue-tattoos${i}`}
+                            style={{ width: '100%' }}
+                            ref={lazyLoadRef}
+                        />
+                    </div>
                 );
-                fetchedImages.push(image);
             }
-
             setImages(fetchedImages);
         };
 
