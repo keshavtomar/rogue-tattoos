50a34ccf0fb6e9378303ad2856472c8006e61622
keshav <tomarkeshavofficial@gmail.com>
2023-07-14 03:14:59 +0530

    toast added, stable client details changed

--- src/components/Getstartedform.jsx
+++ src/components/Getstartedform.jsx
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/getstartedform.css'
 import Loader from './Loader'
-import { useNavigate } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 import Brand from './Brand';
 import MouseFollowingEyes from './MouseFollowingEyes';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 export default function Getstartedform() {
+    const toastId = React.useRef(null);
+    const notify = () => {
+        toastId.current = toast("Sending your Request", {
+            autoClose: false,
+            closeButton: false
+        });
+        setTimeout(() => {
+            toast.update(toastId.current, {
+                render: () => <div>Error Connecting, Call the artist instead</div>,
+                type: "error",
+                autoClose: 2000,
+                closeButton: null
+            })
+        }, 8000);
+    }
+
+    const update = (ob) => {
+        toast.update(toastId.current, {
+            render: () => <div>{ob.message}</div>,
+            type: ob.type,
+            autoClose: 1000,
+            closeButton: null
+        });
+    }
     const [formData, setFormData] = useState({
         fullname: "",
         email: "",
@@ -15,9 +40,9 @@ export default function Getstartedform() {
         isWhatsApp: "",
         message: "",
         date: "",
-        time: "",
+        time: ""
     })
-    const navigate = useNavigate();
+
     const setVal = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     }
@@ -33,7 +58,7 @@ export default function Getstartedform() {
     }
 
     const handleScroll = () => {
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
         const followUsElement = document.querySelector('tav');
 
@@ -77,6 +102,7 @@ export default function Getstartedform() {
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
+        notify();
         const date = new Date();
         const currDate = date.getDate().toString().padStart(2, '0') + "/" + (date.getMonth() + 1).toString().padStart(2, '0') + "/" + date.getFullYear().toString();
         const currTime = date.getHours().toString().padStart(2, '0') + ":" + date.getMinutes().toString().padStart(2, '0');
@@ -93,7 +119,7 @@ export default function Getstartedform() {
             body: JSON.stringify(formData),
         });
 
-        const response = await data.json().then(console.log(data));
+        const response = await data.json();
 
 
         if (response.message === "successs") {
@@ -105,12 +131,11 @@ export default function Getstartedform() {
                 message: "",
                 date: "",
                 time: "",
-            });
-            console.log("Data successfully submitted");
-            navigate("/");
+            })
+            update({ message: "Form Submitted Successfully", type: "success" });
         }
         else {
-            console.log("Not done");
+            update({ message: "Error!, Try again", type: "error" })
         }
 
     }
@@ -141,15 +166,15 @@ export default function Getstartedform() {
                     <MouseFollowingEyes />
                 </div>
                 <div className="form-control">
-                    <input type="text" required name='fullname' value={formData.value} onChange={setVal} />
+                    <input type="text" required name='fullname' value={formData.fullname} onChange={setVal} />
                     <label>Full&nbsp;name</label>
                 </div>
                 <div className="form-control">
-                    <input type="number" required name='mobile' value={formData.value} onChange={setVal} />
+                    <input type="number" required name='mobile' value={formData.mobile} onChange={setVal} />
                     <label>Mobile&nbsp;no.</label>
                 </div>
                 <div className="form-control">
-                    <input type="text" required />
+                    <input type="email" name='email' value={formData.email} onChange={setVal} required />
                     <label>Email&nbsp;id</label>
                 </div>
                 <div className="form-control" style={{ display: 'flex', justifyContent: 'flex-end', alignContent: 'flex-end' }}>
@@ -157,11 +182,12 @@ export default function Getstartedform() {
                     <input className='chkbox' checked={isWhatsappAllowed} onChange={handleCheckboxchange} style={{ height: '20px', width: '20px', position: 'relative', top: '13px', backgroundColor: isWhatsappAllowed ? '#a68954' : '', display: 'flex', justifyContent: 'flex-end' }} type="checkbox" />
                 </div>
                 <div className="form-control" style={{ width: '85%' }}>
-                    <input type="text" required name='message' value={formData.value} onChange={setVal} />
+                    <input type="text" required name='message' value={formData.message} onChange={setVal} />
                     <label>Write&nbsp;a&nbsp;Message&nbsp;for&nbsp;us</label>
                 </div>
                 <button className='form-submit-button' onClick={handleFormSubmit}>Get a Callback</button>
             </div>
+            <ToastContainer position='bottom-left' theme='dark' className="ToastContainer" />
         </div>
     )
 }
